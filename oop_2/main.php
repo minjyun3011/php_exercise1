64037566c8b84fb50910bb98c10718e6c848d511
minjyun3011 <grand3canyon10@gmail.com>
2023-11-17 19:47:56 +0900

    乗車人数制限(定員4人)と残りの乗車可能人数の処理も追加！

--- oop_2/main.php
+++ oop_2/main.php
@@ -24,3 +24,20 @@ echo PHP_EOL;
 // 2人降車
 // 1人しか乗っていないので降車できない
 echo $taxi1->lower(2);
+echo PHP_EOL;
+
+// 4人いっきに乗車
+// 定員は4人なので乗車できない
+echo $taxi1->pickup(4);
+echo PHP_EOL;
+
+echo $taxi1->pickup(2);
+echo $taxi1->information();
+echo PHP_EOL;
+
+echo $taxi1->pickup(2);
+echo PHP_EOL;
+
+echo $taxi1->pickup(1);
+echo $taxi1->information();
+echo PHP_EOL;
--- oop_2/taxi.php
+++ oop_2/taxi.php
@@ -55,9 +55,20 @@ class taxi extends car
 
     public function pickup($passenger)
     {
-        $this->passenger += $passenger;
-        return "{$passenger}人乗車しました" . PHP_EOL;
+        if ($passenger + $this->passenger <= 4) {
+            $this->passenger += $passenger;
+            return "{$passenger}人乗車しました" . PHP_EOL;
+
+            // if ($passenger + $this->passenger == 4) {
+            //     return "定員に達しました" . PHP_EOL;
+            // } else {
+            //     return "残り{strval(4 - $this->passenger)}人乗車可能です" . PHP_EOL;
+            // }
+        } else {
+            return "{$passenger}人は乗車できません" . PHP_EOL;
+        }
     }
+
 
     public function lower($passenger)
     {
@@ -76,11 +87,17 @@ class taxi extends car
 
     public function information()
     {
-        return
-            "車の車種: {$this->name}" . PHP_EOL .
-            "車体番号: {$this->number}" . PHP_EOL .
-            "カラー: {$this->color}" . PHP_EOL .
-            "乗車人数: {$this->total()}人" . PHP_EOL;
+        $message = "車の車種: {$this->name}\n" .
+            "車体番号: {$this->number}\n" .
+            "カラー: {$this->color}\n" .
+            "乗車人数: {$this->total()}人\n";
+
+        if ($this->total() < 4) {
+            $message .= "残り" . (4 - $this->total()) . "人乗車可能です\n";
+        } else {
+            $message .= "定員に達しました\n";
+        }
+
+        return $message;
     }
 }
-
